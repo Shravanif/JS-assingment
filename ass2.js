010634efdaa3c2501c06fcf81b8782c77cb3e052
Shravanif <65101868+Shravanif@users.noreply.github.com>
2020-07-18 14:57:51 +0530

    Add files via upload

---
+++ ass2.js
@@ -0,0 +1,41 @@
+
+// -------------Homework------------------
+//-----------------Assingement 2-------------
+let text  ="the quick brown box jump over the wall";
+let arr=["apple","banana","grapes"];
+const str ="LetsUpgrade";
+
+console.log(text);
+console.log(text.length);
+console.log(text.toLocaleLowerCase());
+console.log(text.toLocaleUpperCase());
+console.log(text.charAt('12'));
+console.log(text.endsWith('wall'));
+console.log(text.indexOf('o'));
+
+//substring
+console.log(str.substring(1,3));
+console.log(str.substring(2));
+
+//slice
+console.log(arr.slice(1,2));
+console.log(text.slice(1,20));
+
+//replace
+console.log(text.replace('box','Song'));
+
+// -------------Assignment 1-----------------
+
+
+let age=prompt('What is your age?');
+confirm('are you over 18?');
+console.log(age);
+
+
+
+
+
+
+
+
+
